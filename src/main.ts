8baeae05726f2fa33ebfb3f0d8f624f2f3e2036a
Jester Lumacad <mr.jesterlumacad@gmail.com>
2024-12-27 16:52:45 +0800

    using deno as runtime for TypeScript

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
+let user: string = prompt("Enter a name: ") || "";
+let greeting: string = prompt("Enter a greeting: ") || "";
 
+function greetHello(greeting: string, user: string): string {
+  let greet = greeting + "" + user;
+  console.log(greet);
+
+  return greet;
+}
+
+greetHello(greeting, user);
